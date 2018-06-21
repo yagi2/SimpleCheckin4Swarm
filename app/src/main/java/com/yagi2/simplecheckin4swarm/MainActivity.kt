3206ac7d4972c890ba8f1c0a81d0ea0e3fa4f280
Itsuki Aoyagi <mail@yagi2.com>
2018-06-21 12:49:31 +0900

    initial commit

---
+++ app/src/main/java/com/yagi2/simplecheckin4swarm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yagi2.simplecheckin4swarm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
