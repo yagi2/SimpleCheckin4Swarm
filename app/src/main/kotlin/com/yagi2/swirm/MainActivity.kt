d3397fd79b27d3f22081787eaede8f1d696b19d2
Itsuki Aoyagi <mail@yagi2.com>
2018-06-25 14:19:56 +0900

    fixed in portrait orientation and change init map fragment method

--- app/src/main/kotlin/com/yagi2/swirm/MainActivity.kt
+++ app/src/main/kotlin/com/yagi2/swirm/MainActivity.kt
@@ -2,21 +2,18 @@ package com.yagi2.swirm
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        (supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment)
-                .getMapAsync(this)
+        initMap()
     }
 
     override fun onMapReady(map: GoogleMap) {
@@ -24,5 +21,21 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         map.addMarker(MarkerOptions().position(tokyo).title("Marker in Tokyo"))
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(tokyo, 15.toFloat()))
+    }
+
+    private fun initMap() {
+        val mapFragment = SupportMapFragment.newInstance(GoogleMapOptions().apply {
+            compassEnabled(false)
+            zoomGesturesEnabled(false)
+            rotateGesturesEnabled(false)
+            scrollGesturesEnabled(false)
+            tiltGesturesEnabled(false)
+        })
+
+        supportFragmentManager.beginTransaction()
+                .add(R.id.map_container, mapFragment)
+                .commit()
+
+        mapFragment.getMapAsync(this)
     }
 }
