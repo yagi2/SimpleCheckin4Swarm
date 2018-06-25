853c6879d3fb82f5041d0c7f9d302f8f478bc032
Itsuki Aoyagi <mail@yagi2.com>
2018-06-25 12:37:14 +0900

    add map to main screen

--- app/src/main/kotlin/com/yagi2/swirm/MainActivity.kt
+++ app/src/main/kotlin/com/yagi2/swirm/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.yagi2.swirm
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment)
+                .getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        val tokyo = LatLng(35.681168, 139.76052)
+
+        map.addMarker(MarkerOptions().position(tokyo).title("Marker in Tokyo"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(tokyo, 15.toFloat()))
     }
 }
