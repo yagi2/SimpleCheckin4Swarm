2c0a376ba3f5a8fa5762310c2c18027a3c07570f
Itsuki Aoyagi <mail@yagi2.com>
2018-06-26 14:20:55 +0900

    add and adopt to foursquare (use oursquare SDK)

--- app/src/main/kotlin/com/yagi2/swirm/MainActivity.kt
+++ app/src/main/kotlin/com/yagi2/swirm/MainActivity.kt
@@ -3,11 +3,14 @@ package com.yagi2.swirm
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.foursquare.android.nativeoauth.FoursquareOAuth
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import permissions.dispatcher.NeedsPermission
@@ -15,6 +18,9 @@ import permissions.dispatcher.RuntimePermissions
 
 @RuntimePermissions
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+
+    private val REQUEST_4SQ_CONNECT = 100
+    private val REQUEST_4SQ_TOKEN_EXCHANGE = 200
 
     private lateinit var currentLatLng: LatLng
     private lateinit var googleMap: GoogleMap
@@ -24,6 +30,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
         setContentView(R.layout.activity_main)
 
         initMap()
+
+        //TODO DEBUGコード、AccessTokenが存在しないならダイアログを出して誘導する
+        startActivityForResult(FoursquareOAuth.getConnectIntent(this, getString(R.string.client_id)), REQUEST_4SQ_CONNECT)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -39,6 +48,20 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
     override fun onLocationChanged(location: Location) {
         currentLatLng = LatLng(location.latitude, location.longitude)
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 16.5f))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_4SQ_CONNECT -> {
+                val code = FoursquareOAuth.getAuthCodeFromResult(resultCode, data)
+                startActivityForResult(FoursquareOAuth.getTokenExchangeIntent(this, getString(R.string.client_id), getString(R.string.client_secret), code.code), REQUEST_4SQ_TOKEN_EXCHANGE)
+            }
+            REQUEST_4SQ_TOKEN_EXCHANGE -> {
+                val token = FoursquareOAuth.getTokenFromResult(resultCode, data)
+                //TODO アクセストークンはSharedPreferenceに保存しておく
+                Toast.makeText(this, "AccessToken : ${token.accessToken}", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     private fun initMap() {
